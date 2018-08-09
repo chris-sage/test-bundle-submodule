3fa6c04da3bd851e38270bf6f8469f2ab3c7330d
Sullivan <Christopher.Sullivan@sage.com>
2018-08-09 14:52:47 +0100

    Modified message on the submodule

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello World from the submodule\n');
 }).listen(8082);
 
 // Console will print the message
